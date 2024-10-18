9b653c662f0aec2977679f3067f64db9fbbefec6
FlorentHajdari <florent.hajdari@hotmail.se>
2024-10-18 14:47:08 +0200

    Kommentarer och kod.

---
+++ IntroOOP/Circle.cs
@@ -0,0 +1,18 @@
+﻿namespace IntroOOP
+{
+    public class Circle
+    {
+        private float _pi = 3.141f; // Variabel för PI
+        private int _Radius; // Variabel för Radie
+
+        public Circle (int radie) // Konstruktor med radie som parameter
+        {
+            _Radius = radie; // Tilldelar radie variabeln värde
+        }
+
+        public double GetArea() // Metod för att räkna ut area
+        {
+            return _Radius * _Radius * _pi; // Beräknar area enligt formel och returnerar svaret
+        }
+    }
+}
--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Circle circle1 = new Circle(5); // Skapar cirkel med radie 5
+            double area1 = circle1.GetArea(); // Beräknar area för cirkeln
+            Console.WriteLine("Cirkeln med radie 5 har en area på: " + area1); // Skriver ut i konsolen
+
+            Circle circle2 = new Circle(6); // Skapar cirkel med radie 6
+            double area2 = circle2.GetArea(); // Beräknar area för cirklen
+            Console.WriteLine("Cirkeln med radie 6 har en area på: " + area2); //Skriver ut i konsolen
         }
     }
 }
